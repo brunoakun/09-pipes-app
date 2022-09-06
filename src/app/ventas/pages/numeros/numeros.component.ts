b5fd08bf42a6b38023c180113cf6cc57e767299b
Bruno Barange <bruno.akun@gmail.com>
2022-09-06 17:44:54 +0200

    pipes de números

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./numeros.component.scss']
 })
 export class NumerosComponent implements OnInit {
+  ventasNetas: number = 2645875.2564;
+  porcentaje: number = 0.547;
+  fecha: Date = new Date();
 
   constructor() { }
 
