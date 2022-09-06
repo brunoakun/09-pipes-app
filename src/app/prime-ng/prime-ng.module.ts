beb4b182343bf5e952e701dba00449ab34c31a60
Bruno Barange <bruno.akun@gmail.com>
2022-09-06 18:48:21 +0200

    singular/plural y plantillas de textos

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -5,12 +5,17 @@ import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import {MenubarModule} from 'primeng/menubar';
+import {FieldsetModule} from 'primeng/fieldset';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   exports: [
     ButtonModule,
     CardModule,
-    MenubarModule
+    MenubarModule,
+    FieldsetModule,
+    BrowserAnimationsModule
   ]
 })
 export class PrimeNgModule { }
--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoComunesComponent implements OnInit {
 
+  //i18nSelect
+  nombre: string = 'Bruno';
+  genero: string = 'masculino';
+
+  generoMap = {
+    'masculino': 'invitarle',
+    'fenemino': 'invitarla'
+  }
+
+  //I18nPluralPipe
+  clientes: string[] = ['Maria','Pedro','Luis'];
+  clientesMap={
+    '=0':'No tenemos ningún cliente',
+    '=1':'Tenemos un cliente',
+    'other':'Tenemos # clientes'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
