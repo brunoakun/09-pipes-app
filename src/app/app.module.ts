591bc567cee54147983689f49284449e93cfcb09
Bruno Barange <bruno.akun@gmail.com>
2022-09-06 17:21:18 +0200

    multi idioma en pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,13 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
 
-// Importar config de idioma español
+
+// Importar config de idioma español y frances
 import localEs from '@angular/common/locales/es-419';
+import localFr from '@angular/common/locales/fr';
 import { registerLocaleData } from '@angular/common';
 registerLocaleData(localEs);
+registerLocaleData(localFr);
 
 
 @NgModule({
